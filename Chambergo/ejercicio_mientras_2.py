7029f5540424f41276968379f48d9ffcb413a6ce
Alicia <lvilchezo@unprg.edu.pe>
2019-12-15 08:49:59 -0500

    mientras 02

---
+++ Chambergo/ejercicio_mientras_2.py
@@ -0,0 +1,9 @@
+# Programa 02
+
+peso= 50
+peso_invalido= (peso < 55 or peso > 70)
+while(peso_invalido):
+    peso=float(input("Ingrese peso"))
+    peso_invalido=(peso < 55 or peso > 70)
+#fin_while
+print("peso valido:", peso)
