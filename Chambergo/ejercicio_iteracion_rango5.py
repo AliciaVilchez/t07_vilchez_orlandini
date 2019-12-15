f3ad6a2d0f1dbf9ee118f78143a7db4312f010c0
Alicia <lvilchezo@unprg.edu.pe>
2019-12-15 09:37:54 -0500

    Iteracion con Rango 05

---
+++ Chambergo/ejercicio_iteracion_rango5.py
@@ -0,0 +1,8 @@
+# Programa 05
+
+costo_unitario=float(input("Precio del medicamento:"))
+for i in range(4):
+    producto=str(input("Ingrese medicamento:"))
+    cantidad_de_medicamento=int(input("Cantidad de medicamento comprado:"))
+    print(producto,"Costo total",(costo_unitario*cantidad_de_medicamento),"Gracias por su preferencia")
+#fin_for
